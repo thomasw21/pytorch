2d375bb6da087a3597c4dfaadd39bbe3e9a01ff4
Jiewen Tan <jwtan@fb.com>
2021-10-05 15:45:58 -0700

    Updated BUILD_ENVIRONMENT condition in test.sh.

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -513,7 +513,7 @@ elif [[ "${BUILD_ENVIRONMENT}" == *libtorch* ]]; then
   # TODO: run some C++ tests
   echo "no-op at the moment"
 elif [[ "${BUILD_ENVIRONMENT}" == *-test1 || "${JOB_BASE_NAME}" == *-test1 || "${SHARD_NUMBER}" == 1 ]]; then
-  if [[ "${BUILD_ENVIRONMENT}" == *linux-xenial-cuda11.3*-test1* ]]; then
+  if [[ "${BUILD_ENVIRONMENT}" == *linux-xenial-cuda11.3* ]]; then
     test_torch_deploy
     test_lazy_tensor_core
   fi
